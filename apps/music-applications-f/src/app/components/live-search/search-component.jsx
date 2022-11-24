baeadc0043f679bc4b357eab6afbaa64cf71c710
evgenyshaikovskiy <evgenyshaikovskiy@gmail.com>
2022-11-24 20:51:26 +0300

    extract code from livesearch

--- apps/music-applications-f/src/app/components/live-search/search-component.jsx
+++ apps/music-applications-f/src/app/components/live-search/search-component.jsx
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { Strategy } from '../../services/parsing-strategy';
 import { RequestParser } from '../../services/request-parser';
 import ApplicationSelect from '../ui-elements/select';
 import InteractiveDropdown from './interactive-dropdown';
@@ -21,55 +20,56 @@ const selectorStyles = {
   width: '100px',
 };
 
-export function LiveSearch() {
+export function LiveSearch({
+  isInputDisabled,
+  isSelectorDefaultValueDisabled,
+  selectorParamsArray,
+  instanceClickCallback,
+  searchWordInitialState,
+  endpointUrl,
+  parsingStrategy,
+}) {
   const [results, setResults] = useState([]);
   const [query, setQuery] = useState('');
-  const [searchWord, setSearchWord] = useState('all');
+  const [searchWord, setSearchWord] = useState(
+    searchWordInitialState.toLowerCase()
+  );
+  const [requestErrorText, setRequestErrorText] = useState('');
 
-  const selectorParamsArray = [
-    { value: 'artist', name: 'Artist' },
-    { value: 'song', name: 'Song' },
-    { value: 'genre', name: 'Genre' },
-    { value: 'album', name: 'Album' },
-  ];
-
-  // consider refactoring this part
-  const searchForAllInstances = 'All';
+  const disableTag = (isDisabled) => (isDisabled ? 'disabled' : '');
 
   useEffect(() => {
     // move request code to different class later
     const searchDelayTimer = setTimeout(() => {
-      if (query !== '') {
-        axios
-          .get(
-            `http://localhost:4200/api/search?${searchWord}=${query.toLowerCase()}`
-          )
-          .then((response) => {
+      if (query !== '' && searchWord !== '') {
+        axios.get(`${endpointUrl}${searchWord}=${query.toLowerCase()}`).then(
+          (response) => {
             setResults(
               RequestParser.parseResponseData(
                 response.data.records,
-                Strategy.ParseObj
+                parsingStrategy
               )
             );
-          });
+          },
+          (reason) => {
+            setRequestErrorText(reason);
+          }
+        );
       }
-    }, 300);
+    }, 400);
 
     return () => clearTimeout(searchDelayTimer);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [query, searchWord]);
 
-  function callback(item) {
-    console.log(`callback for this ${item}`);
-    console.log(searchWord);
-    console.log(item);
-  }
-
+  // need to style empty divs and when promise is rejected and when result is empty
   return (
     <div className="livesearch-wrapper">
       <div className="livesearch-input-container">
         <ApplicationSelect
-          defaultValueName={searchForAllInstances}
-          defaultValue={searchForAllInstances.toLowerCase()}
+          isDefaultDisabled={disableTag(isSelectorDefaultValueDisabled)}
+          defaultValueName={searchWordInitialState}
+          defaultValue={searchWordInitialState.toLowerCase()}
           className="livesearch-selector"
           value={searchWord}
           onChange={(value) => setSearchWord(value)}
@@ -77,6 +77,7 @@ export function LiveSearch() {
           options={selectorParamsArray}
         ></ApplicationSelect>
         <input
+          disabled={disableTag(isInputDisabled)}
           className="livesearch-input"
           value={query}
           type="text"
@@ -87,12 +88,12 @@ export function LiveSearch() {
       {query !== '' ? (
         <div className="dropdown-container">
           <InteractiveDropdown
-            onClickCallback={callback}
+            onClickCallback={instanceClickCallback}
             list={results}
           ></InteractiveDropdown>
         </div>
       ) : (
-        <div></div>
+        <div>{requestErrorText}</div>
       )}
     </div>
   );
--- apps/music-applications-f/src/app/pages/search.jsx
+++ apps/music-applications-f/src/app/pages/search.jsx
@@ -1,12 +1,39 @@
 import LiveSearch from '../components/live-search/search-component';
+import { Strategy } from '../services/parsing-strategy';
 
 export function SearchPage() {
+  // props to pass
+  const selectorParamsArray = [
+    { value: 'artist', name: 'Artist' },
+    { value: 'song', name: 'Song' },
+    { value: 'genre', name: 'Genre' },
+    { value: 'album', name: 'Album' },
+  ];
+  const endpointUrl = 'http://localhost:4200/api/search?';
+  const parsingStrategy = Strategy.ParseGraphDbObj;
+  const searchWordInitialState = 'All';
+
+  // callbacks to pass
+  function callbackOnInstanceClick(instance) {
+    console.log(`callback for this ${instance}`);
+    console.log(instance);
+  }
+
   return (
     <div className="search-page-wrapper">
-      <div className='search-page-title'>
-        <p>Type to search...</p>
+      <div className="search-page-title">
+        <p>Type to search from graph database...</p>
       </div>
-        <LiveSearch></LiveSearch>
+      <LiveSearch
+        isInputDisabled={false}
+        selectorParamsArray={selectorParamsArray}
+        instanceClickCallback={callbackOnInstanceClick}
+        isSelectorDefaultValueDisabled={false}
+        defaultSelectorValue={searchWordInitialState}
+        searchWordInitialState={searchWordInitialState}
+        endpointUrl={endpointUrl}
+        parsingStrategy={parsingStrategy}
+      ></LiveSearch>
     </div>
   );
 }
