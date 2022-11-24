50f51239c17d52693942cbe5a00d3aebfc1e10be
evgenyshaikovskiy <evgenyshaikovskiy@gmail.com>
2022-11-24 21:05:52 +0300

    add base for web livesearch

--- apps/music-applications-f/src/app/pages/search-web.jsx
+++ apps/music-applications-f/src/app/pages/search-web.jsx
@@ -1,8 +1,29 @@
+import { useState } from 'react';
+
 export function SearchWebPage() {
+  const urlToLogin = 'http://localhost:4200/api/login';
+
+  const [buttonState, setButtonState] = useState('');
+  const [isSearchingEnabled, setSearchingState] = useState(false);
+
+  function onUnlockButtonClick() {
+    setSearchingState(true);
+    setButtonState('disabled');
+
+    window.open(urlToLogin, '_blank');
+
+    console.log(isSearchingEnabled);
+  }
+
   return (
     <div className="search-web-page-wrapper">
       <div className="search-web-page-title">
-        <p>Type to search...</p>
+        <p>Type to search from web...</p>
+      </div>
+      <div className="search-web-conn-block">
+        <button onClick={onUnlockButtonClick} disabled={buttonState}>
+          Click to unlock searching
+        </button>
       </div>
     </div>
   );
