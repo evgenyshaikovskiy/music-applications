ca170732fec592126a59258f9d65d6c18747b168
evgenyshaikovskiy <evgenyshaikovskiy@gmail.com>
2022-11-29 02:20:10 +0300

    add item page

--- apps/music-applications-f/src/app/pages/item.jsx
+++ apps/music-applications-f/src/app/pages/item.jsx
@@ -1,12 +1,55 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import { Strategy } from '../services/parsing-strategy';
+import { ResponseParser } from '../services/response-parser';
 
-function ItemPage({ item }) {
+function ItemPage() {
   const router = useNavigate();
   const params = useParams();
 
+  const [item, setItem] = useState({ });
+
+  const recognizeParsingStrategy = (type) => {
+    switch (type) {
+      case 'track':
+        return Strategy.ParseSpotifyTrack;
+      case 'artist':
+        return Strategy.ParseSpotifyArtist;
+      case 'album':
+        return Strategy.ParseSpotifyAlbum;
+      case 'playlist':
+        return Strategy.ParseSpotifyPlaylist;
+      default:
+        return undefined;
+    }
+  };
+
+  const parsingStrategy = recognizeParsingStrategy(params.type);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4200/api/${params.type}/${params.id}`)
+      .then((response) => {
+        setItem({
+          ...item,
+          ...ResponseParser.parseResponseData(response.data, parsingStrategy),
+        });
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [params.id, params.type]);
+
   return (
-    <div>
-      {params.id} {params.type}
+    <div className="item-page-wrapper">
+      <div className="item-page-btns">
+        <button onClick={() => router(-1)} className="go-back-btn">
+          Back
+        </button>
+        <button className="save-to-db-btn">Save to db</button>
+      </div>
+      <div className="item-page-content">
+
+      </div>
     </div>
   );
 }
