cf840c777ad9f6841bc271223ffd117307f77611
evgenyshaikovskiy <evgenyshaikovskiy@gmail.com>
2022-11-29 02:20:25 +0300

    add parsing methods

--- apps/music-applications-f/src/app/services/parsing-strategy.js
+++ apps/music-applications-f/src/app/services/parsing-strategy.js
@@ -67,4 +67,103 @@ export const Strategy = {
         });
     }
   },
+  ParseSpotifyTrack: function (track) {
+    return {
+      type: track.type,
+      label: track.name,
+      spotify_id: track.id,
+      explicit: track.explicit,
+      duration_ms: track.duration_ms,
+      artists: track.artists.map((artist) => {
+        return { label: artist.name, spotify_id: artist.id, type: artist.type };
+      }),
+      album: {
+        spotify_id: track.album.id,
+        type: track.album.type,
+        images: track.album.images,
+        label: track.album.name,
+        album_type: track.album.album_type,
+        release_date: track.album.release_date,
+        total_tracks: track.album.total_tracks,
+      },
+    };
+  },
+  ParseSpotifyArtist: function (artist) {
+    return {
+      genres: artist.genres,
+      type: artist.type,
+      spotify_id: artist.id,
+      label: artist.name,
+      images: artist.images,
+    };
+  },
+  ParseSpotifyPlaylist: function (playlist) {
+    return {
+      spotify_id: playlist.id,
+      images: playlist.images,
+      name: playlist.name,
+      collaborative: playlist.collaborative,
+      type: playlist.type,
+      tracks_num: playlist.tracks.total,
+      tracks: playlist.tracks.items.slice(0, -1).map((value) => {
+        return {
+          type: value['track'].type,
+          label: value['track'].name,
+          spotify_id: value['track'].id,
+          explicit: value['track'].explicit,
+          duration_ms: value['track'].duration_ms,
+          track_num: value['track'].track_number,
+          artists: value['track'].artists.map((artist) => {
+            return {
+              label: artist.name,
+              spotify_id: artist.id,
+              type: artist.type,
+            };
+          }),
+          album: {
+            spotify_id: value['track'].album.id,
+            type: value['track'].album.type,
+            label: value['track'].album.name,
+            album_type: value['track'].album.album_type,
+          },
+        };
+      }),
+    };
+  },
+  ParseSpotifyAlbum: function (album) {
+    return {
+      spotify_id: album.id,
+      type: album.type,
+      album_type: album.album_type,
+      release_date: album.release_date,
+      tracks_num: album.total_tracks,
+      label: album.name,
+      actual_label: album.label,
+      images: album.images,
+      tracks: album.tracks.items.map((track) => {
+        return {
+          type: track.type,
+          label: track.name,
+          spotify_id: track.id,
+          explicit: track.explicit,
+          duration_ms: track.duration_ms,
+          track_num: track.track_number,
+          artists: track.artists.map((artist) => {
+            return {
+              label: artist.name,
+              spotify_id: artist.id,
+              type: artist.type,
+            };
+          }),
+        };
+      }),
+      artist: album.artists.map((artist) => {
+        return {
+          label: artist.name,
+          spotify_id: artist.id,
+          type: artist.type,
+        };
+      }),
+    };
+  },
 };
