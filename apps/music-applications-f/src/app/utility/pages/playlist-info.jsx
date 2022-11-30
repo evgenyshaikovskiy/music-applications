59970c13d07c400185ae9d0bc8d454308889bff8
Kirill <kirill@.com>
2022-11-30 23:50:12 +0300

    popa

--- apps/music-applications-f/src/app/utility/pages/playlist-info.jsx
+++ apps/music-applications-f/src/app/utility/pages/playlist-info.jsx
@@ -3,39 +3,58 @@ import { convertDuration } from '../utils';
 const PlaylistInfo = ({ playlist }) => {
   return (
     <div className="item-page-content">
-      <div className="item-page-title">
-        <p>
-          Currently viewing {playlist.type} {playlist.name}
-        </p>
-      </div>
-      <div className="item-page-details">
-        <p>Playlist is {playlist.collaborative ? '' : 'not'} collaborative</p>
-        <p>Playlist contains {playlist.tracks.length} tracks</p>
-        <p>
-          Featured tracks:
-          {/* needs to be fixed by giving key props for each child */}
-          {playlist.tracks.map((track) => {
-            return (
-              <div>
-                <p>
-                  {track.artists.map((artist) => artist.label).join(' ')} - {' '}
-                  {track.label} album {track.album.label}
-                </p>
-                <p>Duration: {convertDuration(track.duration_ms)}</p>
-                <p>Explicit: {track.explicit ? 'Yes' : 'No'}</p>
+      <div>
+        <div className="playlist-item-page-details-header">
+            <div className="playlist-item-page-images">
+              <img
+                src={playlist.images[0].url}
+                height='300px'
+                width='300px'
+                alt="playlist-cover"
+              ></img>
+            </div>
+              <div className="playlist-item-page-title">
+                <div className="playlist-type-text">
+                  <p>
+                    PLAYLIST
+                  </p>
+                </div>
+                <div className="playlist-label-text">
+                  <p>
+                    {playlist.name}
+                  </p>
+                </div>
+                <div className="playlist-info-text">
+                  <p>
+                    {' '} 
+                    Playlist is {playlist.collaborative ? '' : 'not'} collaborative
+                  </p>
+                  <p className='playlist-symbol'>&#9679;</p>
+                  <p>
+                    Playlist contains {playlist.tracks.length} tracks
+                  </p>
+                </div> 
               </div>
-            );
-          })}
-        </p>
-      </div>
-      <div className="item-page-details">
-        <img
-          src={playlist.images[0].url}
-          height={playlist.images[0].height}
-          width={playlist.images[0].width}
-          alt="playlist-cover"
-        ></img>
-        <p>Playlist cover</p>
+          </div>
+        
+        <div>
+          <p className='playlist-feature-track-label'>Featured tracks:</p>
+            {playlist.tracks.map((track) => {
+              return (
+                <div className='playlist-track-text'>
+                  <p>
+                    <span className='playlist-leftstr'>{playlist.tracks.indexOf(track)+1}.{track.artists.map((artist) => artist.label).join(' ')} - {' '}
+                    {track.label}</span>
+                    <span className='playlist-rightstr'>album{':'} {track.album.label}</span>
+                  </p>
+                  <p>
+                    <span className='playlist-leftstr'>Duration: {convertDuration(track.duration_ms)}</span>
+                    <span className='playlist-rightstr'>Explicit: {track.explicit ? 'Yes' : 'No'}</span>
+                  </p>
+                </div>
+              );
+            })}
+        </div>
       </div>
     </div>
   );
