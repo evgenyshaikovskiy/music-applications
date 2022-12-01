b3c277eb9bcb3f39ed3993ef1250a2ab13dbacb2
Kirill <kirill@.com>
2022-12-01 22:28:07 +0300

    add new artist page

--- apps/music-applications-f/src/app/utility/pages/artist-info.jsx
+++ apps/music-applications-f/src/app/utility/pages/artist-info.jsx
@@ -3,23 +3,26 @@ const ArtistInfo = ({ artist }) => {
   // fetch all playlist? ??? ? ?
   return (
     <div className="item-page-content">
-      <div className="item-page-title">
-        <p>
-          {' '}
-          Currently viewing {artist.type} {artist.label}
-        </p>
-      </div>
-      <div className="item-page-details">
-        <p>Artist performs in these genres: {artist.genres.join(', ')}</p>
-      </div>
-      <div className="item-page-images">
-        <img
-          src={artist.images[1].url}
-          height={artist.images[1].height}
-          width={artist.images[1].width}
-          alt="artist-cover"
-        ></img>
-        <p>Artist image</p>
+      <div className="artist-item-page-details-header">
+        <div className="artist-item-page-image">
+          <img
+              src={artist.images[1].url}
+              height={artist.images[1].height}
+              width={artist.images[1].width}
+              alt="artist-cover"
+          ></img>
+        </div>
+        <div className="artist-item-page-title">
+          <div className="artist-type-text">
+            <p>{artist.type.toUpperCase()}</p>
+          </div>
+          <div className="artist-label-text">
+            <p>{artist.label}</p>
+          </div>
+          <div className="artist-genres-text">
+            <p>{artist.genres.join(' ● ').toUpperCase()}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
