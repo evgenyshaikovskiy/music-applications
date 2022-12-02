59b5c2033905881eb97d4334822ff9b99d59de25
evgenyshaikovskiy <evgenyshaikovskiy@gmail.com>
2022-12-02 15:14:59 +0300

    inject service

--- apps/music-applications-b/src/app/controllers/network.controller.ts
+++ apps/music-applications-b/src/app/controllers/network.controller.ts
@@ -1,7 +1,10 @@
+import { HttpService } from '@nestjs/axios';
 import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('network')
 export class NetworkController {
+  constructor(private readonly httpService: HttpService) {}
+
   @Get('comment/:commentText')
   async postComment(@Param() params) {
     console.log('received comment', params.commentText);
