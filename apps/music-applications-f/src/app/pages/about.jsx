7a1d0eb76c1fc36d181b113fca70950847071c6b
Kirill <kirill@.com>
2022-12-03 01:20:02 +0300

    add about page

--- apps/music-applications-f/src/app/pages/about.jsx
+++ apps/music-applications-f/src/app/pages/about.jsx
@@ -1,6 +1,95 @@
 export function AboutPage() {
-  return <div>ABOUT PAGE</div>;
+  return (
+    <div className="about-item-header-text">
+      <div className="about-item-name-command-text">Над проектом работали:</div>
+      <div className="about-item-command">
+        <div className="about-item-people">
+          <div className="about-item-people-text">&#129332; 1.Шайковский Е.Н.</div>
+          <div className="about-item-peple-img">
+            <img
+              src="https://sun9-north.userapi.com/sun9-79/s/v1/ig2/O1XV4DCbBUF-pyF1pt-5aYzXFe_S90MrhnKcppC9uRvU9ZynJuLoGqZbr1a7FnawN9ypU8mAV2V7v3hxNEen2tD-.jpg?size=960x1280&quality=96&type=album"
+              height={300}
+              width={300}
+              alt="Шашковский"
+            ></img>
+          </div>
+        </div>
+        <div className="about-item-people">
+          <div className="about-item-people-text">&#129321; 2.Дятел К.Д.</div>
+          <div className="about-item-peple-img">
+            <img
+              src="https://sun9-east.userapi.com/sun9-26/s/v1/ig2/uSkE-5F0QoqBJ91pss1Bj1oLsDXizrREf6X25r_OXggzjsOQc0nPvg5x7ssO43EGjg2Dv9D3cN7sKGz2NQO4SPH-.jpg?size=960x1280&quality=96&type=album"
+              height={300}
+              width={300}
+              alt="Малой"
+            ></img>
+          </div>
+        </div>
+        <div className="about-item-people">
+          <div className="about-item-people-text">&#128526; 3.Карачун И.В.</div>
+          <div className="about-item-peple-img">
+            <img
+              src="https://sun1.beltelecom-by-minsk.userapi.com/s/v1/ig2/whcgxDytpM-BhHcQ2Q8DnP6QpX6yekXoTm_oguOcAZ_xZfdvaZLZahOjEBACoMTIa7sA-9bLpE5Nkp618yG6Cd_l.jpg?size=880x1506&quality=96&type=album"
+              height={300}
+              width={300}
+              alt="Хуесос"
+            ></img>
+          </div>
+        </div>
+        <div className="about-item-people">
+          <div className="about-item-people-text">&#129313; 4.Старостин П.</div>
+        </div>
+        <div className="about-item-people">
+          <div className="about-item-people-text">
+            &#129314; 5.Рассафонов С.Д.
+          </div>
+          <div className="about-item-peple-img">
+            <img
+              src="https://sun9-west.userapi.com/sun9-13/s/v1/ig2/okxEkwG8UxltCRhjGLosama_BWg8sQOU7sSVEr1oueSi1kFWbGrWuNI-mDz1sTFOOA4YuphpqGOk1QT12XwESt_a.jpg?size=1080x1250&quality=96&type=album"
+              height={300}
+              width={300}
+              alt="Уже нет на проекте"
+            ></img>
+          </div>
+        </div>
+        <div className="about-item-people">
+          <div className="about-item-people-text">&#129312; 6.Лаптев К.</div>
+          <div className="about-item-peple-img">
+            <img
+              src="https://sun9-north.userapi.com/sun9-85/s/v1/ig2/u89LNf4BLc7-ei7kk-0frhx4m2-Sk8BMmw_zoIzoZwGpL6oMM4rVIBFVKJ_pVVJGEVlrYILHDY3-SEvdQfJclbn2.jpg?size=2560x1920&quality=96&type=album"
+              height={300}
+              width={300}
+              alt=""
+            ></img>
+          </div>
+        </div>
+        <div className="about-item-people">
+          <div className="about-item-people-text">&#129299; 7.Самчук Е.</div>
+          <div className="about-item-peple-img">
+            <img
+              src="https://cdn.ebs.newsner.com/wp-content/uploads/sites/2/2018/06/Hund-2.jpg"
+              height={300}
+              width={300}
+              alt="Ботан ебаный"
+            ></img>
+          </div>
+        </div>
+        <div className="about-item-people">
+          <div className="about-item-people-text">
+            &#128128; 8.Селедцов Е.Д.
+          </div>
+          <div className="about-item-peple-img">
+            <img
+              src="https://sun9-north.userapi.com/sun9-78/s/v1/ig2/H0cic1NuNnyOs27H0dN_Zbwwe9h98eaMywiB7ZPOXk3y367XkDu8uBjltvT22cPYwcfQ9Y1KAn6oDEEdeRp-W6aJ.jpg?size=960x1280&quality=96&type=album"
+              height={300}
+              width={300}
+              alt="Дылда"
+            ></img>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AboutPage;
-  
