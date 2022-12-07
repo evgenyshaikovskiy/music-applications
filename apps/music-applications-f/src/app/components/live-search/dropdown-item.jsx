9457fb5036b61b6b1647f9e1e15c6f3c73d989b5
evgenyshaikovskiy <evgenyshaikovskiy@gmail.com>
2022-12-07 18:53:10 +0300

    bebra

--- apps/music-applications-f/src/app/components/live-search/dropdown-item.jsx
+++ apps/music-applications-f/src/app/components/live-search/dropdown-item.jsx
@@ -1,18 +1,12 @@
 const InteractiveItemList = ({ item, onItemClickCallback }) => {
   console.log(item);
 
-  function capitalize(string) {
-    if (string) {
-      return string.charAt(0).toUpperCase() + string.slice(1);
-    }
-  }
-
   // consider using different print strategies for different objects
   return (
     <div className="clickable-dropdown-item">
       <div className="clickable-dropdown-item-text">
         <div className="dropdown-item-label">{item.label}</div>
-        <div className="dropdown-item-type">{capitalize(item.type)}</div>
+        <div className="dropdown-item-type">{item.type}</div>
       </div>
       <button
         className="view-details-btn"
