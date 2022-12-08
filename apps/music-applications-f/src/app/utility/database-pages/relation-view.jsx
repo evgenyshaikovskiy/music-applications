c02eed37ad6ca67c14deac7e69a2be1d147b6a84
Kirill <kirill@.com>
2022-12-08 03:16:12 +0300

    graph page(no wokring)

--- apps/music-applications-f/src/app/utility/database-pages/relation-view.jsx
+++ apps/music-applications-f/src/app/utility/database-pages/relation-view.jsx
@@ -1,17 +1,9 @@
 const RelationViewPage = ({ item }) => {
   console.log(item);
-  /*{album.tracks.map((track, index) => {
-    return (
-      <AlbumTrackInfo
-        track={track}
-        key={index}
-        onTrackClickCallback={trackNameClickCallback}
-        onArtistClickCallback={artistNameClickCallback}
-      />
-    );
-  })} */
   return (
     <div className="database-item-page-text">
+      {/* {item.relations.length} */}
+      <div className="database-item-name-text">{item.properties.name.toUpperCase()}</div>
       {item.relations.map((relation, index) => {
         switch (relation.type) {
           case 'Author':
@@ -19,14 +11,35 @@ const RelationViewPage = ({ item }) => {
               <AuthorRelation
                 target={relation.target}
                 key={index}
+                item_type={item.type}
+                relation_type={relation.type}
               ></AuthorRelation>
             );
           case 'AppearedAt':
-            return <AppearedAtRelation></AppearedAtRelation>;
+            return (
+              <AppearedAtRelation
+                target={relation.target}
+                key={index}
+                item_type={item.type}
+                relation_type={relation.type}
+              ></AppearedAtRelation>
+            );
           case 'PerformsInGenre':
-            return (<PerfomsInGenreRelation target={relation.target} key={index}></PerfomsInGenreRelation>);
+            return (
+              <PerfomsInGenreRelation
+                target={relation.target}
+                key={index}
+                item_type={item.type}
+              ></PerfomsInGenreRelation>
+            );
           case 'Contains':
-            return <ContainsRelation></ContainsRelation>;
+            return (
+              <ContainsRelation
+                target={relation.target}
+                key={index}
+                item_type={item.type}
+              ></ContainsRelation>
+            );
           default:
             return <div></div>;
         }
@@ -37,29 +50,74 @@ const RelationViewPage = ({ item }) => {
 
 export default RelationViewPage;
 
-const AuthorRelation = ({ target }) => {
-  return (
+const AuthorRelation = ({ target, item_type, relation_type }) => {
+  switch (target.type) {
+    case 'Track':
+      return (
+        <div className="database-item-author-text">
+          {target.properties.name}
+          {' - '}
+          {relation_type}{"("}{}
+        </div>
+      );
+    case 'Album':
+      return (
+        <div className="database-item-author-text">
+          {target.properties.name}
+          {' - '}
+          {relation_type}
+        </div>
+      );
+    case 'Artist':
+      return (
+        <div className="database-item-author-text">
+          {target.properties.name}
+          {' - '}
+          {relation_type}
+        </div>
+      );
+  }
+  /*   return (
     <div>
       {target.type === 'Track' ? (
         <div>{target.properties.name}</div>
       ) : (
-        <div>{/*if album*/}</div>
+        <div>{if album}</div>
+      )}
+    </div>
+  ); */
+};
+
+const AppearedAtRelation = ({ target, item_type, relation_type }) => {
+  return (
+    <div>
+      {target.properties.name}
+      {' - '}
+      {target.type}{" ("}{relation_type}{")"}
+    </div>
+  );
+};
+
+const PerfomsInGenreRelation = ({ target, item_type }) => {
+  return (
+    <div>
+      {item_type === 'Genre' ? (
+        <div className="database-item-perfomsin-genre-artists-text">
+          {/* add index */}{target.properties.name}{' - '}{target.type}
+        </div>
+      ) : (
+        <div className="database-item-perfomsin-artist-genres-text">{target.properties.name}{' - '}{target.type}</div>
       )}
     </div>
   );
 };
 
-const AppearedAtRelation = ({ target }) => {
-  return <div></div>;
-};
-
-const PerfomsInGenreRelation = ({ target }) => {
+const ContainsRelation = ({ target, item_type }) => {
   return (
-  <div>
-    {target.properties.name}
-  </div>);
-};
-
-const ContainsRelation = ({ target }) => {
-  return <div></div>;
+    <div className="database-item-contains-text">
+      {target.properties.name}
+      {' - '}
+      {target.type}
+    </div>
+  );
 };
