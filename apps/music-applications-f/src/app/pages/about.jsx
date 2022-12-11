c049fd494468987232b6ee6f67669c73e025e9a1
Eramaloby <karachun04@gmail.com>
2022-12-11 21:48:40 +0300

    fixed about page, changed members pics

--- apps/music-applications-f/src/app/pages/about.jsx
+++ apps/music-applications-f/src/app/pages/about.jsx
@@ -9,7 +9,7 @@ export function AboutPage() {
           </div>
           <div className="about-item-peple-img">
             <img
-              src="https://sun9-north.userapi.com/sun9-79/s/v1/ig2/O1XV4DCbBUF-pyF1pt-5aYzXFe_S90MrhnKcppC9uRvU9ZynJuLoGqZbr1a7FnawN9ypU8mAV2V7v3hxNEen2tD-.jpg?size=960x1280&quality=96&type=album"
+              src="https://sun9-14.userapi.com/impg/8CVR5sPKwy4NkM3vSr8ZBgXeOk2H89n5qV6BEQ/fATs3PmjyYM.jpg?size=960x1280&quality=96&sign=2454c7c3eda243200612fd1671e0d1ed&type=album"
               height={300}
               width={300}
               alt="Шашковский"
@@ -20,7 +20,7 @@ export function AboutPage() {
           <div className="about-item-people-text">&#129321; 2.Дятел К.Д.</div>
           <div className="about-item-peple-img">
             <img
-              src="https://sun9-east.userapi.com/sun9-26/s/v1/ig2/uSkE-5F0QoqBJ91pss1Bj1oLsDXizrREf6X25r_OXggzjsOQc0nPvg5x7ssO43EGjg2Dv9D3cN7sKGz2NQO4SPH-.jpg?size=960x1280&quality=96&type=album"
+              src="https://psv4.userapi.com/c237131/u204889733/docs/d26/80b99e93e023/kirilldyatel.jpg?extra=ymhw70zjYIFNXH9AUhZt0knV4kDCSeyuW-urT7envgRbE0pIJQhGmzOjEdx6vKtio9NnH2LdzlQo2egt-lefl4i18x0gr3wgfzQLizTBV9T5P72s2NAUUpD8b-px-hhUY8-fCl7OilUDiauUwn5m661rg5o"
               height={300}
               width={300}
               alt="Малой"
@@ -31,7 +31,7 @@ export function AboutPage() {
           <div className="about-item-people-text">&#128526; 3.Карачун И.В.</div>
           <div className="about-item-peple-img">
             <img
-              src="https://sun1.beltelecom-by-minsk.userapi.com/s/v1/ig2/whcgxDytpM-BhHcQ2Q8DnP6QpX6yekXoTm_oguOcAZ_xZfdvaZLZahOjEBACoMTIa7sA-9bLpE5Nkp618yG6Cd_l.jpg?size=880x1506&quality=96&type=album"
+              src="https://sun9-34.userapi.com/impg/3aQOetFPwKSJZfuz4G_9enPv6Yo9_DaZnNGoJw/e85gIAqOjG8.jpg?size=606x1280&quality=95&sign=d47fc63d589892911acd70d3096608de&type=album"
               height={300}
               width={300}
               alt="Вантус"
@@ -39,15 +39,10 @@ export function AboutPage() {
           </div>
         </div>
         <div className="about-item-people">
-          <div className="about-item-people-text">&#129313; 4.Старостин П.</div>
-        </div>
-        <div className="about-item-people">
-          <div className="about-item-people-text">
-            &#129314; 5.Рассафонов С.Д.
-          </div>
+          <div className="about-item-people-text">&#129313; 4.Старостин П.Г.</div>
           <div className="about-item-peple-img">
             <img
-              src="https://sun9-west.userapi.com/sun9-13/s/v1/ig2/okxEkwG8UxltCRhjGLosama_BWg8sQOU7sSVEr1oueSi1kFWbGrWuNI-mDz1sTFOOA4YuphpqGOk1QT12XwESt_a.jpg?size=1080x1250&quality=96&type=album"
+              src="https://sun9-1.userapi.com/impg/Ia0NpTitDRu0Cc97kYM24U1CXbvwy-l3vnXw9A/ALvmN-El81M.jpg?size=960x1280&quality=96&sign=d28a851ac6bf38f7fa067b4d52805e4d&type=album"
               height={300}
               width={300}
               alt="Уже нет на проекте"
@@ -55,10 +50,23 @@ export function AboutPage() {
           </div>
         </div>
         <div className="about-item-people">
-          <div className="about-item-people-text">&#129312; 6.Лаптев К.</div>
+          <div className="about-item-people-text">
+            &#129314; 5.Рассафонов С.Д.
+          </div>
           <div className="about-item-peple-img">
             <img
-              src="https://sun9-north.userapi.com/sun9-85/s/v1/ig2/u89LNf4BLc7-ei7kk-0frhx4m2-Sk8BMmw_zoIzoZwGpL6oMM4rVIBFVKJ_pVVJGEVlrYILHDY3-SEvdQfJclbn2.jpg?size=2560x1920&quality=96&type=album"
+              src="https://sun9-68.userapi.com/impg/s7S0TPvPxVTnbxGuvQh_eV2565YnMs_dLLLiKg/Wu2AWvbanaQ.jpg?size=853x1280&quality=96&sign=6b69d6b125872fde706b332aeb34820c&type=album"
+              height={300}
+              width={300}
+              alt="Уже нет на проекте"
+            ></img>
+          </div>
+        </div>
+        <div className="about-item-people">
+          <div className="about-item-people-text">&#129312; 6.Лаптев К.Д.</div>
+          <div className="about-item-peple-img">
+            <img
+              src="https://sun9-55.userapi.com/impf/c853424/v853424550/df666/w2OPuwrtm8s.jpg?size=997x1500&quality=96&sign=fa55325922a0a667f9b2cbef03eaed52&type=album"
               height={300}
               width={300}
               alt=""
@@ -66,10 +74,10 @@ export function AboutPage() {
           </div>
         </div>
         <div className="about-item-people">
-          <div className="about-item-people-text">&#129299; 7.Самчук Е.</div>
+          <div className="about-item-people-text">&#129299; 7.Самчук Е.У.</div>
           <div className="about-item-peple-img">
             <img
-              src="https://cdn.ebs.newsner.com/wp-content/uploads/sites/2/2018/06/Hund-2.jpg"
+              src="https://psv4.userapi.com/c237131/u204889733/docs/d54/7bba66cf9d78/zhenyasamchuk.jpg?extra=w46hKtUHzODE_d45_IkVIhMu8NH1TxN0uTVA_NnNOMx2e8rixd4PHWiiJmrMg-CMDYvbiQlAwBFEP4IG6qObLfJkktB6mRtVuDeKUN9SHeEapw_A8x6L2aS_q6ULigaZciaBAl9cags5pYxrLt1cNdnKsno"
               height={300}
               width={300}
               alt="Ботик"
@@ -82,7 +90,7 @@ export function AboutPage() {
           </div>
           <div className="about-item-peple-img">
             <img
-              src="https://sun9-north.userapi.com/sun9-78/s/v1/ig2/H0cic1NuNnyOs27H0dN_Zbwwe9h98eaMywiB7ZPOXk3y367XkDu8uBjltvT22cPYwcfQ9Y1KAn6oDEEdeRp-W6aJ.jpg?size=960x1280&quality=96&type=album"
+              src="https://sun9-28.userapi.com/impg/7tbuJqdfk_83rwKmO3M-Pr0lUvk5yyh0X6ijXg/V1i3PhlqpOE.jpg?size=1215x2160&quality=96&sign=c81e9751a0c828801cff29d329e512ae&type=album"
               height={300}
               width={300}
               alt="Дылда"
